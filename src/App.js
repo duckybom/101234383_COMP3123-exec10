dc9e7030f2e27c6d699619e684c50f53016a81b0
Minh Duc Cung <64608402+duckybom@users.noreply.github.com>
2021-11-15 17:36:19 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DataEntryForm from './DataEntryForm';
+
+function App() {
+  return (
+    <div className="App">
+      <DataEntryForm/>
+    </div>
+  );
+}
+
+export default App;
